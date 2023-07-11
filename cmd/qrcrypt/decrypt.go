a4d5912311e8f4b18dcb6c1c4cf3d2e5c1198ad5
Philipp Leuenberger <leuenbrgr@gmail.com>
2023-07-11 23:15:11 +0200

    qr code generation

--- cmd/qrcrypt/decrypt.go
+++ cmd/qrcrypt/decrypt.go
@@ -1,7 +1,10 @@
 package qrcrypt
 
 import (
+	b64 "encoding/base64"
 	"fmt"
+	"os"
+
 	"github.com/LeuenbergerP/qrcrypt/pkg/qrcrypt"
 	"github.com/spf13/cobra"
 )
@@ -12,10 +15,42 @@ var decryptCmd = &cobra.Command{
 	Short:   "Decrypt a string",
 	Args:    cobra.ExactArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
+		fileName := args[0]
 		keyPhrase := args[1]
-		res := qrcrypt.Decrypt([]byte(args[0]), keyPhrase)
+		content, err := readFile(fileName)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		res := qrcrypt.Decrypt(content, keyPhrase)
 		fmt.Println(string(res))
 	},
+}
+
+func readFile(filename string) ([]byte, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	fi, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+	data := make([]byte, fi.Size())
+	_, err2 := f.Read(data)
+	if err2 != nil {
+		return nil, err2
+	}
+	b64Decoded, err3 := base64Decode(data)
+	if err3 != nil {
+		return nil, err3
+	}
+	return b64Decoded, nil
+}
+
+func base64Decode(data []byte) ([]byte, error) {
+	return b64.StdEncoding.DecodeString(string(data))
 }
 
 func init() {
--- cmd/qrcrypt/encrypt.go
+++ cmd/qrcrypt/encrypt.go
@@ -1,8 +1,14 @@
 package qrcrypt
 
 import (
+	"bytes"
+	b64 "encoding/base64"
 	"fmt"
+	"log"
+	"os"
+
 	"github.com/LeuenbergerP/qrcrypt/pkg/qrcrypt"
+	"github.com/mdp/qrterminal/v3"
 
 	"github.com/spf13/cobra"
 )
@@ -11,12 +17,51 @@ var encryptCmd = &cobra.Command{
 	Use:     "encrypt",
 	Aliases: []string{"enc"},
 	Short:   "Encrypt a string",
-	Args:    cobra.ExactArgs(2),
+	Args:    cobra.ExactArgs(3),
 	Run: func(cmd *cobra.Command, args []string) {
-		keyPhrase := args[1]
-		res := qrcrypt.Encrypt([]byte(args[0]), keyPhrase)
-		fmt.Println(string(res))
+		fileName := args[0]
+		content := args[1]
+		keyPhrase := args[2]
+		res := qrcrypt.Encrypt([]byte(content), keyPhrase)
+		writeToFile(res, fileName)
 	},
+}
+
+func writeToFile(data []byte, filename string) error {
+	f, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	defer f.Close()
+	encoded := base64Encode(data)
+	qrCode := qrCode(encoded)
+	fmt.Println(qrCode)
+	fmt.Println(encoded)
+	_, err2 := f.WriteString(encoded)
+	if err2 != nil {
+		log.Fatal(err2)
+		return err2
+	}
+	return nil
+}
+
+func base64Encode(data []byte) string {
+	return b64.StdEncoding.EncodeToString([]byte(data))
+}
+
+func qrCode(val string) string {
+	buf := new(bytes.Buffer)
+	config := qrterminal.Config{
+		Level:      qrterminal.L,
+		Writer:     buf,
+		HalfBlocks: false,
+		BlackChar:  qrterminal.WHITE,
+		WhiteChar:  qrterminal.BLACK,
+		QuietZone:  4,
+	}
+	qrterminal.GenerateWithConfig(val, config)
+	return buf.String()
 }
 
 func init() {
